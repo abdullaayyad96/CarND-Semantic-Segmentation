c302ab742c9a46f66c3fbdb7eff9337990b0cf06
Abdulla Ayyad <abdulla.ayyad96@gmail.com>
2019-04-11 16:32:57 +0900

    add scripts to download dataset and trained NN model

---
+++ data/download_dataset.sh
@@ -0,0 +1,2 @@
+curl -O https://s3.eu-central-1.amazonaws.com/avg-kitti/data_road.zip
+unzip data_road.zip
---
+++ model/download_model.sh
@@ -0,0 +1,3 @@
+wget --no-check-certificate "https://onedrive.live.com/download?cid=F9787260D5A284A5&resid=F9787260D5A284A5%21221888&authkey=AP0q1s7e3mIwmUg"
+mv download?cid=F9787260D5A284A5&resid=F9787260D5A284A5%21221888&authkey=AP0q1s7e3mIwmUg model.zip
+unzip model.zip
